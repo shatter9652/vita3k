362e88cac91d997280b5f5baaf0760876c56b125
shatter9652 <shatterstar9652@gmail.com>
2026-06-20 20:38:50 -0400

    fix (cache too)

--- cmake/vcpkg_ios.cmake
+++ cmake/vcpkg_ios.cmake
@@ -1,2 +1,7 @@
+set(VCPKG_TARGET_TRIPLET arm64-ios)
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../external/vcpkg/scripts/toolchains/ios.cmake")
+
+# Help vcpkg ios toolchain detect architecture
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+
 include("${CMAKE_CURRENT_LIST_DIR}/../external/vcpkg/scripts/buildsystems/vcpkg.cmake")
