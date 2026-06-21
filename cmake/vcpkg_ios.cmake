889de170bdf97dcc943528f1a4234464e6ffdcc3
shatter9652 <shatterstar9652@gmail.com>
2026-06-20 20:23:19 -0400

    fuckie

--- cmake/vcpkg_ios.cmake
+++ cmake/vcpkg_ios.cmake
@@ -1,3 +1,2 @@
-set(VCPKG_TARGET_TRIPLET arm64-ios)
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../external/vcpkg/scripts/toolchains/ios.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/../external/vcpkg/scripts/buildsystems/vcpkg.cmake")
